b81c09f99249d0e84a4bfcae866030f7a951e022
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2021-12-19 03:43:40 +0200

    Controlpanel Admin Design Finish

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const htmlwebpackplugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-
+const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin');
 
 module.exports = {
     entry: {
@@ -34,12 +34,7 @@ module.exports = {
         [
             {
                 test: /\.html$/,
-                use:
-                [
-                    {
-                        loader: 'html-loader',
-                    }
-                ]
+                use: [{ loader: 'html-loader' }]
             },
             {
                 test: /\.(sa|sc|c)ss$/,
@@ -54,23 +49,23 @@ module.exports = {
                 test: /\.m?js$/,
                 exclude: /node_modules/,
                 use: {
-                  loader: "babel-loader",
-                  options: {
-                    presets: ['@babel/preset-env']
-                  }
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
             },
             {
                 test: /\.(png|svg|jpe?g|gif)$/,
                 exclude: /fonts/,
                 use: [
-                  {
-                    loader: "file-loader", 
-                    options: {
-                      name: '[name].[ext]',
-                      outputPath: "assets/images",
+                    {
+                        loader: "file-loader", 
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: "assets/images",
+                        }
                     }
-                  }
                 ]
             },
             {
@@ -93,21 +88,43 @@ module.exports = {
         new htmlwebpackplugin({
             filename: "index.html",
             template: "./src/index.html",
+            chunks: ['app','assets/js/banner','assets/js/tabs','assets/js/chart']
+        }),
+        new htmlwebpackplugin({
+            filename: "add-product.html",
+            template: "./src/add-product.html",
             chunks: ['app']
         }),
-        
+        new htmlwebpackplugin({
+            filename: "products.html",
+            template: "./src/products.html",
+            chunks: ['app']
+        }),
+        new htmlwebpackplugin({
+            filename: "users.html",
+            template: "./src/users.html",
+            chunks: ['app']
+        }),
+        new htmlwebpackplugin({
+            filename: "add-user.html",
+            template: "./src/add-user.html",
+            chunks: ['app','assets/js/upload']
+        }),
+        new htmlwebpackplugin({
+            filename: "orders.html",
+            template: "./src/orders.html",
+            chunks: ['app']
+        }),
         new htmlwebpackplugin({
             filename: "components/button.html",
             template: "./src/components/button.html",
             chunks: ['app']
         }),
-
         new htmlwebpackplugin({
             filename: "components/textfield.html",
             template: "./src/components/textfield.html",
             chunks: ['app']
         }),
-
         new htmlwebpackplugin({
             filename: "components/card.html",
             template: "./src/components/card.html",
@@ -158,6 +175,7 @@ module.exports = {
             template: "./src/components/table.html",
             chunks: ['app']
         }),
+
         new htmlwebpackplugin({
             filename: "components/charts.html",
             template: "./src/components/charts.html",
@@ -170,7 +188,26 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: "assets/css/styles.css",
         }),
-
+        new HtmlWebpackPartialsPlugin({
+            path: path.join(__dirname, './src/components/sidebar.html'),
+            location: 'sidebar',
+            template_filename: ['index.html','add-product.html','products.html','users.html','orders.html','add-user.html'],
+        }),
+        new HtmlWebpackPartialsPlugin({
+            path: path.join(__dirname, './src/components/banner.html'),
+            location: 'banner',
+            template_filename: ['index.html'],
+        }),
+        new HtmlWebpackPartialsPlugin({
+            path: path.join(__dirname, './src/components/charts.html'),
+            location: 'charts',
+            template_filename: ['index.html'],
+        }),
+        new HtmlWebpackPartialsPlugin({
+            path: path.join(__dirname, './src/components/help.html'),
+            location: 'help',
+            template_filename: ['index.html','add-product.html','products.html','users.html','orders.html','add-user.html'],
+        }),
 
     ]
 }
